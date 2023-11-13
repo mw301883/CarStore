f0e8cec53cb259ac74771a1fd227eec4c549a621
Michał Wieczorek <mw301883@student.polsl.pl>
2023-11-13 16:41:58 +0100

    Add paramietrized test

--- src/test/java/michal/wieczorek/carstore/Model/User/UserTest.java
+++ src/test/java/michal/wieczorek/carstore/Model/User/UserTest.java
@@ -4,8 +4,12 @@
  */
 package michal.wieczorek.carstore.Model.User;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  *
@@ -15,15 +19,24 @@ public class UserTest {
     
     public UserTest() {
     }
-
+    
+    static Stream<Arguments> dataProvider() {
+    return Stream.of(
+            Arguments.of(new User(" ", " ", " ", " ", " ", " ", UserEnum.UserType.STANDARD), 
+                          new User(" ", " ", " ", " ", " ", " ", UserEnum.UserType.PREMIUM))
+    );
+}
+    
     /**
      * Test of areCarListsEmpty method, of class User.
      */
-    @Test
-    public void testAreCarListsEmpty() {
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void testAreCarListsEmpty(User usrStd, User usrPrm) {
         System.out.println("areCarListsEmpty");
-        User instance = new User(" ", " ", " ", " ", " ", " ", UserEnum.UserType.PREMIUM);
-        assertEquals(true, instance.areCarListsEmpty(), "Unable to detect if car list is empty");
+        assertEquals(true, usrStd.areCarListsEmpty(), "Unable to detect if car list is empty - return that is not empty but it is.");
+        usrPrm.addCarAtoOrder(0);
+        assertEquals(false, usrPrm.areCarListsEmpty(), "Unable to detect if car list is empty - return that is  empty but it is not.");
     }
     
 }
